755318c420674dd42219331cf536f331a3edac9e
Rejda <qati.rejda@gmail.com>
2024-11-09 21:24:40 +0100

    models

--- Ecommercevet-backend/app/Models/OrderProduct.php
+++ Ecommercevet-backend/app/Models/OrderProduct.php
@@ -2,9 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class OrderProduct extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'order_products';
+
+    protected $fillable = [
+        'product_id',
+        'order_id',
+        'quantity',
+        'size',
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
 }
