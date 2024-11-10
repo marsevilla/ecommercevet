d9f03ce7be09df3172e626aa31b2325c4c515d55
Maria del Mar Sevilla Escobar <sevillamariadelmar@gmail.com>
2024-11-10 15:04:19 +0100

    header done

--- Ecommercevet/src/Components/Header.jsx
+++ Ecommercevet/src/Components/Header.jsx
@@ -1,4 +1,25 @@
+import React from 'react'
+import './Header.css';
+
 function Header() {
-  return <p>Ceci est un Header</p>;
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="Logo.webp" alt="Coeur de soie" />
+        <span className="brand-name">Coeur de soie</span>
+      </div>
+      <nav className="nav">
+        <ul>
+          <li><a href="/">Accueil</a></li>
+          <li><a href="/boutique">Boutique</a></li>
+          <li><a href="/contact"></a>Contact</li>
+        </ul>
+      </nav>
+      <div className="header-icons">
+        <a href="/account" className="icon"><i className="fa fa-user"></i></a>
+        <a href="/cart"><i className="fa fa-shopping-cart"></i></a>
+      </div>
+    </header>
+  );
 }
 export default Header;
