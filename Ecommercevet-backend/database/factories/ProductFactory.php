964a9111448e29588928c9794a0a1bf62c86f523
Sophie Théréau <sophiethereau@hotmail.fr>
2024-11-11 11:31:14 +0100

    Ajout d'une short_description et d'une image au niveau des produits en back

--- Ecommercevet-backend/database/factories/ProductFactory.php
+++ Ecommercevet-backend/database/factories/ProductFactory.php
@@ -19,7 +19,9 @@ class ProductFactory extends Factory
     {
         return [
             'name' => $this->faker->word,
+            'short_description' => $this->faker->sentence,
             'description' => $this->faker->sentence,
+            'image_url' => $this->faker->imageUrl(640, 480, 'product', true, 'Faker'), 
             'category' => $this->faker->randomElement(['haut', 'bas', 'accessoires']),
             'quantity' => $this->faker->numberBetween(1, 300),
             'size' => json_encode($this->faker->randomElements(['S', 'M', 'L', 'XL'], $this->faker->numberBetween(1, 4))),        ];
