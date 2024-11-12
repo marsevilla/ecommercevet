564660aa887651a947fa98a744f434beac2d1bb3
Sophie Théréau <sophiethereau@hotmail.fr>
2024-11-12 09:54:38 +0100

    Installation tailwind

--- Ecommercevet/src/main.jsx
+++ Ecommercevet/src/main.jsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
+import './Styles/main.scss';
 //import '@fortawesome/fontawesome-free/css/all.css';
 
 createRoot(document.getElementById("root")).render(
