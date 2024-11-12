d771a6c05bd50427eeabae62a023379491829425
Sophie Théréau <sophiethereau@hotmail.fr>
2024-11-12 14:52:09 +0100

    Lien du login et du register avec le back

--- Ecommercevet-backend/app/Http/Controllers/UserController.php
+++ Ecommercevet-backend/app/Http/Controllers/UserController.php
@@ -6,5 +6,83 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function showProfile()
+    {
+        $user = auth()->user();
+
+        return response()->json([
+            'user' => $user,
+            'isAdmin' => $user->isAdmin()
+        ]);
+    }
+
+
+    public function updateProfile(Request $request)
+    {
+        $user = Auth::user();
+
+        $validatedData = $request->validate([
+            'name' => 'string|max:255',
+            'email' => 'string|email|max:255|unique:users,email,' . $user->id,
+        ]);
+
+        $user->update($validatedData);
+
+        return response()->json([
+            'user' => $user
+        ]);
+    }
+
+
+    public function index()
+    {
+        $user = Auth::user();
+
+        if (!$user->isAdmin()) {
+            return response()->json(['error' => 'Accès refusé. Vous devez être administrateur.'], 403);
+        }
+
+        $users = User::all();
+
+        return response()->json([
+            'users' => $users
+        ]);
+    }
+
+    public function show($id)
+    {
+        $user = Auth::user();
+
+        if (!$user->isAdmin()) {
+            return response()->json(['error' => 'Accès refusé. Vous devez être administrateur.'], 403);
+        }
+
+        $user = User::findOrFail($id);
+
+        return response()->json([
+            'user' => $user
+        ]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $user = Auth::user();
+        if (!$user->isAdmin()) {
+            return response()->json(['error' => 'Accès refusé. Vous devez être administrateur.'], 403);
+        }
+        $userToUpdate = User::findOrFail($id);
+
+        $validatedData = $request->validate([
+            'name' => 'string|max:255',
+            'email' => 'string|email|max:255|unique:users,email,' . $userToUpdate->id,
+            'role' => 'in:admin,user',
+        ]);
+
+        $userToUpdate->update($validatedData);
+
+        return response()->json([
+            'user' => $userToUpdate
+        ]);
+    }
+
 }
